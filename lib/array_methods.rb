604ae01b4afd7591154e72eb9dc27192ecc291c0
Dexter <dex429.dc@gmail.com>
2020-12-16 19:17:13 -0500

    commit tree a76adbb5f80b312b64569a9293846e5b329856b6

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,34 @@
 def using_include(array, element)
+    array.include?(element)
 
 end
 
 def using_sort(array)
+    array = ["wow", "I", "kool", "method"]
+    array.sort
 
 end
 
 def using_reverse(array)
+    array = ["wow", "I", "kool", "arrays!"]
+    array.reverse
 
 end
 
 def using_first(array)
+    array = ["wow", "I", "kool", "method"]
+    array.first
 
 end
 
 def using_last(array)
+    array = ["wow", "I", "kool", "arrays!"]
+    array.last
 
 end
 
 def using_size(array)
+    array = ["wow", "I", "kool", "method", "arrays", "stuff"]
+    array.size
 
 end
